d1aa7da6594e5ba5331f4cc06215fb0a2bc17cdd
Ahn <gan_jinan@qq.com>
2019-07-14 23:43:05 +0800

    [refactor][remove some original codes]

--- main/main.js
+++ main/main.js
@@ -14,10 +14,10 @@ const decodeBarcodes = (tags) => {
         if (tag.indexOf('-') > -1) {
             // 分割条码和数量
             let tagStrings = tag.split('-');
-            count = barcodeMap.get(tagStrings[0]) == undefined ? parseFloat(tagStrings[1]) : barcodeMap.get(tagStrings[0]) + parseFloat(tagStrings[1]);
+            count = barcodeMap.get(tagStrings[0]) === undefined ? parseFloat(tagStrings[1]) : barcodeMap.get(tagStrings[0]) + parseFloat(tagStrings[1]);
             barcodeMap.set(tagStrings[0], count);
         } else {
-            count = barcodeMap.get(tag) == undefined ? 1 : barcodeMap.get(tag) + 1;
+            count = barcodeMap.get(tag) === undefined ? 1 : barcodeMap.get(tag) + 1;
             barcodeMap.set(tag, count);
         }
     });
@@ -98,87 +98,8 @@ const printReceipt = (tags) => {
     let items = decodeTags(tags);
     let receiptItems = calculateReceiptItems(items);
     let receipt = calculateReceipt(receiptItems);
-    let renderReceiptResult = renderReceipt(receipt);
-    return renderReceiptResult;
+    return renderReceipt(receipt);
 };
-
-
-
-const handleMapToItemList = (barcodeMap) => {
-    const allItems = loadAllItems();
-    let itemList = [];
-    barcodeMap.forEach((value, key) => {
-        let filterItem = allItems.filter(val => {
-            return val.barcode == key;
-        });
-        let itemObj = filterItem[0];
-        // itemObj['subAmount'] = filterItem[0].price * value;
-        itemObj['count'] = value;
-        itemList.push(itemObj);
-    });
-    return itemList;
-}
-
-const handleItemAmount = (itemList, promotionsDetail) => {
-    const promotionsBarcodes = promotionsDetail[0].barcodes;
-    itemList.forEach(item => {
-
-        if (promotionsBarcodes.indexOf(item.barcode) > -1 && item.count > 2) {
-            // 买二送一
-            item['subAmount'] = (item.price * (item.count - parseInt(item.count / 3))).toFixed(2);
-        } else {
-            // 原价
-            item['subAmount'] = (item.price * item.count).toFixed(2);
-        }
-    });
-    return itemList;
-}
-
-const calculateTotalAmount = (itemList) => {
-    let totalAmount = 0;
-    itemList.forEach(item => {
-        totalAmount += parseFloat(item.subAmount);
-    });
-    return totalAmount.toFixed(2);
-}
-
-const calculateSaveAmount = (itemList) => {
-    let saveAmount = 0;
-    itemList.forEach(item => {
-        saveAmount += item.price * item.count - item.subAmount;
-    });
-    return saveAmount.toFixed(2);
-}
-
-const renderItemDetail = (itemList) => {
-    let itemDetails = '';
-    itemList.forEach(item => {
-        itemDetails += '名称：' + item.name + '，数量：' + item.count + item.unit + '，单价：' + item.price.toFixed(2) + '(元)，小计：' + item.subAmount + '(元)\n';
-
-    });
-    return itemDetails;
-}
-
-const renderTotalDetail = (totalAmount, saveAmount) => {
-    return '总计：' + totalAmount + '(元)\n节省：' + saveAmount + '(元)';
-}
-
-// const renderReceipt = (itemList) => {
-//     let receipt = '***<没钱赚商店>收据***\n';
-//     const promotionsDetail = loadPromotions();
-//
-//     let detailItemList = handleItemAmount(itemList, promotionsDetail);
-//     let totalAmount = '' + calculateTotalAmount(detailItemList);
-//     let saveAmount = '' + calculateSaveAmount(detailItemList);
-//     let itemDetails = renderItemDetail(detailItemList);
-//     let totalDetails = renderTotalDetail(totalAmount, saveAmount);
-//     receipt += itemDetails;
-//     receipt += '----------------------\n';
-//     receipt += totalDetails;
-//     receipt += '\n**********************';
-//     return receipt;
-// }
-
 
 module.exports = {
     decodeBarcodes,
@@ -190,12 +111,5 @@ module.exports = {
     calculateReceiptSaving,
     calculateReceipt,
     renderReceipt,
-    handleMapToItemList,
-    handleItemAmount,
-    calculateTotalAmount,
-    calculateSaveAmount,
-    renderItemDetail,
-    renderTotalDetail,
-    // renderReceipt,
     printReceipt
 };
--- test.js
+++ test.js
@@ -1,23 +1,9 @@
 const Main = require('./main/main');
 const Api = require('./test/fixtures');
 
-// const loadAllItems = Api.loadAllItems;
-
 const loadPromotions = Api.loadPromotions;
 
 const decodeBarcodes = Main.decodeBarcodes;
-
-const handleMapToItemList = Main.handleMapToItemList;
-
-const handleItemAmount = Main.handleItemAmount;
-
-const calculateTotalAmount = Main.calculateTotalAmount;
-
-const calculateSaveAmount = Main.calculateSaveAmount;
-
-const renderItemDetail = Main.renderItemDetail;
-
-const renderTotalDetail = Main.renderTotalDetail;
 
 const renderReceipt = Main.renderReceipt;
 
@@ -36,9 +22,6 @@ const calculateReceiptTotal = Main.calculateReceiptTotal;
 const calculateReceiptSaving = Main.calculateReceiptSaving;
 
 const calculateReceipt = Main.calculateReceipt;
-
-
-// test for function isStartSmallerThanOrEqualToEnd
 
 // pass
 
@@ -192,7 +175,6 @@ it('should return receiptItems when given items and promotions', () => {
 });
 
 it('should return receiptItems when given items and items', () => {
-    // const promotions = loadPromotions();
 
     const items = [
         {
@@ -444,120 +426,4 @@ it('should return receipt when given barcodes', () => {
 
     expect(printReceipt(tags)).toBe(expectText);
 });
-
-//===============================================================
-
-// pass
-
-it('should return itemList when given itemMap', () => {
-    let itemMap = new Map();
-    itemMap.set("ITEM000001", 5);
-    itemMap.set("ITEM000003", 2.5);
-    itemMap.set("ITEM000005", 3);
-
-    const result = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3}];
-
-    expect(JSON.stringify(handleMapToItemList(itemMap))).toBe(JSON.stringify(result));
-});
-
-
-// pass
-
-it('should return itemList with subAmount when given itemList', () => {
-    const promotionsDetail = loadPromotions();
-
-    const itemList = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3}
-    ];
-
-    const expectValue = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5, "subAmount": 12.00.toFixed(2)},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5, "subAmount": 37.50.toFixed(2)},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3, "subAmount": 9.00.toFixed(2)}
-    ];
-
-    expect(JSON.stringify(handleItemAmount(itemList, promotionsDetail))).toBe(JSON.stringify(expectValue));
-});
-
-
-// pass
-it('should return totalAmount when given itemList', () => {
-
-    const itemList = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5, "subAmount": 12.00.toFixed(2)},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5, "subAmount": 37.50.toFixed(2)},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3, "subAmount": 9.00.toFixed(2)}
-    ];
-
-    expect(calculateTotalAmount(itemList)).toBe(58.5.toFixed(2));
-});
-
-
-// pass
-
-it('should return saveAmount when given itemList', () => {
-
-    const itemList = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5, "subAmount": 12.00.toFixed(2)},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5, "subAmount": 37.50.toFixed(2)},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3, "subAmount": 9.00.toFixed(2)}
-    ];
-
-    expect(calculateSaveAmount(itemList)).toBe(7.5.toFixed(2));
-});
-
-// pass
-
-it('should return itemDetails when given itemList', () => {
-
-    const itemList = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5, "subAmount": 12.00.toFixed(2)},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5, "subAmount": 37.50.toFixed(2)},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3, "subAmount": 9.00.toFixed(2)}
-    ];
-
-    const result = '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-        '名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)\n' +
-        '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n';
-
-    expect(renderItemDetail(itemList)).toBe(result);
-});
-
-// pass
-
-it('should return totalDetails when given total and save amounts', () => {
-
-    const result = '总计：58.50(元)\n节省：7.50(元)';
-
-    expect(renderTotalDetail('58.50', '7.50')).toBe(result);
-});
-
-
-// pass
-it('should return receipt when given itemList', () => {
-
-    const itemList = [
-        {"barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "count": 5},
-        {"barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "count": 2.5},
-        {"barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "count": 3}
-    ];
-    const expectText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：58.50(元)
-节省：7.50(元)
-**********************`;
-
-    expect(renderReceipt(itemList)).toBe(expectText);
-});
-
-// pass
-
-
+// end
